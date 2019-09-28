da25d8d98983d14d940c87c97a9ad82637979ae6
K3be <ahmed.13.alkaaby@gmail.com>
2019-09-28 14:01:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,5 @@
 
 main.remove();
 <<<<<<< HEAD
-var newHeader = document.getElementById("victory").nodeName;
-=======
-var newHeader= document.querySelector("h1");
->>>>>>> 06d17a8ff2ea95abcf0f844108426405c66f8bb1
+let newHeader=document.querySelector("h1#victory");
+newHeader.innerHTML="is the champion"; 
