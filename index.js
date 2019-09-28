23dea77770227aa675c1aeea81b28471040601ef
K3be <ahmed.13.alkaaby@gmail.com>
2019-09-28 14:00:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Write your code here!
 
 main.remove();
+<<<<<<< HEAD
 var newHeader = document.getElementById("victory").nodeName;
+=======
+var newHeader= document.querySelector("h1");
+>>>>>>> 06d17a8ff2ea95abcf0f844108426405c66f8bb1
